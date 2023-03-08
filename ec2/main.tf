4c5cd6d2fe4ae409414d1f67b7784f38cc1eb1c2
Aswanidevm <maswanidev@gmail.com>
2023-03-08 11:42:50 +0300

    automation

--- ec2/main.tf
+++ ec2/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_route53_record" "record" {
   zone_id = "Z04818282BOE8RVGV13K7"
-  name    = "${var.component}.${var.env}.myprojecdevops.info"
+  name    = "${var.component}-${var.env}.myprojecdevops.info"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
